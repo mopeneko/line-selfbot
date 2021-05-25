5e0a57e46c222b95175f7088671c233dafff13d4
mopeneko <mopeneko@lem0n.cc>
2021-05-25 23:51:42 +0900

    GOAWAY発生時に再試行するようにした
    該当エラーは `http.http2GoAwayError` と外部パッケージからアクセスできないため

--- linebot/poll/poll.go
+++ linebot/poll/poll.go
@@ -26,6 +26,9 @@ func Poll(ctx context.Context, client *lineclient.LINEClient) {
 	for {
 		operations, err := client.TalkServiceClientForPolling.FetchOps(ctx, localRev, count, globalRev, individualRev)
 		if err != nil {
+			if strings.HasPrefix(err.Error(), "http2: server sent GOAWAY and closed the connection") {
+				continue
+			}
 			log.Fatalf("failed to call fetchOps: %+v\n", err)
 		}
 
