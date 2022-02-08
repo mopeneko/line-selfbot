fc6d397fbc0394b3b00475aded990c4cb12e19c2
mopeneko <mopeneko@lem0n.cc>
2022-02-09 05:36:30 +0900

    読み込みを実装

--- linebot/cmd/linebot/main.go
+++ linebot/cmd/linebot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	config2 "github.com/mopeneko/line-selfbot/linebot/config"
@@ -17,6 +18,25 @@ func main() {
 
 	ctx := context.Background()
 
+	mid := flag.String("mid", "bot account mid", "use authToken if not expired")
+
+	flag.Parse()
+
+	if *mid != "" {
+		config, err := config2.LoadConfig(*mid)
+		if err != nil {
+			log.Fatalf("failed to load config: %+v", err)
+		}
+
+		client, err := generateLINEClient(conf, config.AuthToken)
+		if err != nil {
+			log.Printf("failed to generate LINE client: %+v", err)
+		}
+
+		Exec(ctx, config.AuthToken, client)
+		return
+	}
+
 	accessToken, certificate, err := androidlite.QRLogin(ctx, conf)
 	if err != nil {
 		log.Fatalf("failed to login: %+v\n", err)
@@ -31,6 +51,10 @@ func main() {
 		log.Fatalf("failed to generate LINE client: %+v\n", err)
 	}
 
+	Exec(ctx, accessToken, client)
+}
+
+func Exec(ctx context.Context, accessToken string, client *lineclient.LINEClient) {
 	// 設定の読み込み
 	config, err := config2.LoadConfig(client.Profile.Mid)
 	if err != nil {
